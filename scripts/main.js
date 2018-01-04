47eb451a2848b6afd9e49a2611b201bc15124d1a
Wilson <epicfishball@gmail.com>
2018-01-05 01:17:45 +0800

    add some sample animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
     $(".startmenu").click(function() {
+        $(".template").animate({
+            "padding": "1.5rem",
+            "margin": "1rem"
+        });
         $(".startmenu").fadeOut();
+        $(".game-content").fadeIn()
     });
 });

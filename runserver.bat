e1ebf66f59baaaeb509193c8b023ae155b185e0f
Wilson <epicfishball@gmail.com>
2018-01-09 21:28:55 +0800

    Turns out if you want to modify a file, you should not commit it -- should look for a better solution

--- runserver.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set DATABASE_URL=postgres://$(whoami)
-set HEROKU_POSTGRESQL_ROSE_URL=postgres://$(whoami)
-set FLASK_APP=webapp.py
-set FLASK_DEBUG=1
-set DEBUG_POSTGRES_PASSWORD=
-flask run

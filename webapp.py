63a9798fc8519a23547c633c2967ef6f2ce9c324
Wilson <epicfishball@gmail.com>
2018-01-09 11:17:09 +0800

    webserver: Add minor restructuring for readability

--- webapp.py
+++ webapp.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, jsonify, make_response, redirect, render_template, request, url_for
-from io import StringIO
 
 app = Flask(__name__)
 
@@ -12,33 +11,37 @@ def main_page():
 @app.route('/database')
 @app.route('/database/<table_name>')
 def debug_database(table_name=None):
-    query_results = None
+    query_results = []
     if table_name:
         #query_results = fetch_table(table_name)
         query_results = [['this', 'is', 'a', 'sample', 'db', 'response']] + [[*'abcdef']] * 500
-    return render_template('debug_database.html', table_name=table_name, query_results=query_results)
+
+    return render_template('debug_database.html', 
+                           table_name=table_name, 
+                           query_results=query_results)
 
 
 @app.route('/database/<table_name>/upload', methods=['GET', 'POST'])
-def debug_database_upload(table_name):
-    if request.method == 'POST':
-        f = request.files['file']
-        print('get file:', f.filename)
-        #upload_table(f)
-        return jsonify([f.filename])
-    return redirect(url_for('debug_database', table_name=table_name))
+def debug_database_upload(table_name=None):
+    if (not table_name) or (request.method == 'GET'):
+        return redirect(url_for('debug_database', table_name=table_name))    
+    
+    f = request.files['file']
+    print('get file:', f.filename)
+    #upload_table(f)
+    return jsonify([f.filename])
 
 
 @app.route('/database/<table_name>/download')
 def debug_database_download(table_name=None):
-    if table_name:
-        #download_table(table_name)
-        pass
+    if (not table_name):
+        return redirect(url_for('debug_database', table_name=table_name))    
     
+    #output = download_table(table_name)
+    output = "foo|bar|baz\neggs|ham|spam"
     csv_fname = table_name + '.csv'
     with open(os.path.join('static', csv_fname), 'w', encoding='utf-8') as f:
-        f.write("foo|bar|baz\neggs|ham|spam")
-        
+        f.write(output)
     return redirect(url_for('static', filename=csv_fname))
 
 

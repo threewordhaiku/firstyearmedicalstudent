de861dbb1645fcc82a7cdacf162f97c224858b66
Justinian <justinian_siah@mymail.sutd.edu.sg>
2018-01-10 20:36:02 +0800

    fix transition effects

--- static/bad_end.js
+++ static/bad_end.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
     $(".nxtbtn").click(function() {
-        $(".template").animate({
-            "padding": "1.5rem",
-            "margin": "1rem"
-        });
         $(".text1").fadeOut();
-        $(".game-content").fadeIn()
+        $(".text2").delay(500).fadeIn();
     });
 });

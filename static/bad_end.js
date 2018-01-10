f9385389c8428ad8332804ce8cb27927b955279f
Justinian <justinian_siah@mymail.sutd.edu.sg>
2018-01-10 21:16:07 +0800

    changed from fading text to appending text

--- static/bad_end.js
+++ static/bad_end.js
@@ -1,6 +1,24 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $(".nxtbtn").click(function() {
         $(".text1").fadeOut();
         $(".text2").delay(500).fadeIn();
     });
+});*/
+
+text = ".text1"
+
+$(document).ready(function() {
+    $(".nxtbtn").click(function() {
+    	text = next_text(text)
+        $(text).fadeIn();
+    });
 });
+
+function next_text(current_text) {
+	next = current_text.slice(-1);
+	nextint = parseInt(next)
+	nextint ++;
+	nextstr = nextint.toString();
+	temp = ".text";
+	return temp.concat(nextstr)
+}

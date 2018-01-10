8dfb576311703d9391b62ec9a986dc987e0e7595
Justinian <justinian_siah@mymail.sutd.edu.sg>
2018-01-10 21:36:42 +0800

    added full bad end 1 story

--- static/bad_end.js
+++ static/bad_end.js
@@ -1,11 +1,5 @@
-/*$(document).ready(function() {
-    $(".nxtbtn").click(function() {
-        $(".text1").fadeOut();
-        $(".text2").delay(500).fadeIn();
-    });
-});*/
-
-text = ".text1"
+text = ".text1";
+counter = 1;
 
 $(document).ready(function() {
     $(".nxtbtn").click(function() {
@@ -16,10 +10,15 @@ $(document).ready(function() {
 });
 
 function next_text(current_text) {
+	if (counter<10){
 	next = current_text.slice(-1);
+	} else {
+	next = current_text.slice(-2);
+	}
 	nextint = parseInt(next)
 	nextint ++;
 	nextstr = nextint.toString();
 	temp = ".text";
+	counter++;
 	return temp.concat(nextstr)
 }

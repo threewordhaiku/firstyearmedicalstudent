582a845cdddf059143dc0cb3b455dddad6e8dead
Justinian <justinian_siah@mymail.sutd.edu.sg>
2018-01-15 01:13:11 +0800

    trying to add -/+ buttons for volume control (not working yet)

--- static/settings.js
+++ static/settings.js
@@ -2,13 +2,29 @@ function show_value(vol_value,vol_id)
 {
  document.getElementById(vol_id).innerHTML=vol_value;
 }
-function add_one()
+function add_one(no,vol_id)
 {
-  document.f.sld6.value=parseInt(document.f.sld6.value)+1;
-  show_value(document.f.sld6.value);
+  slider_id = check(no);
+  var x = $(slider_id).slider("value");
+  x= x+1;
+  $(slider_id).slider("value",x);
+  show_value(x,vol_id);
 }
-function subtract_one()
+
+function check(no){
+    if (no==1){
+        return ".master_volume"
+    }
+    else if (no==2){
+        return ".sfx"
+    }
+    else if (no==3){
+        return ".bgm"
+    }
+}
+
+/*function subtract_one()
 {
-  document.f.sld6.value=parseInt(document.f.sld6.value)-1;
-  show_value(document.f.sld6.value);
-}
+  document.f.master_volume.value=parseInt(document.f.master_volume.value)-1;
+  show_value(document.f.master_volume.value,master_volume);
+}*/

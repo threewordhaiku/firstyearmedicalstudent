2da1464e11a6b085357b075fbd32452fe02bba34
Justinian <justinian_siah@mymail.sutd.edu.sg>
2018-01-15 17:16:07 +0800

    added new_account_page.html, styles and links

--- static/main_page.js
+++ static/main_page.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".SMB").click(function() {
+    $(".SMB_new").click(function() {
         $(".template").animate({
             "padding": "1.5rem",
             "margin": "1rem"

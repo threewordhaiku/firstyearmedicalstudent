7af5ffee1725d981296d255de2a2849dcfaaffcb
Justinian <justinian_siah@mymail.sutd.edu.sg>
2018-01-15 17:42:45 +0800

    cleaned up main_page.js. fixed misaligned button in new_accounts_page.html

--- static/main_page.js
+++ static/main_page.js
@@ -8,20 +8,4 @@ $(document).ready(function() {
         $(".settings").fadeOut();
         $(".game-content").fadeIn()
     });
-
-    $(".settingsbtn").click(function() {
-   		$("a[rel]").overlay({
-        mask: 'darkred',
-        effect: 'apple',
-
-        onBeforeLoad: function() {
-            // grab wrapper element inside content
-            var wrap = this.getOverlay().find(".contentWrap");
-            // load the page specified in the trigger
-            wrap.load(this.getTrigger().attr("href"));
-        }
- 
-    	});
-	});
- })
-
+});
